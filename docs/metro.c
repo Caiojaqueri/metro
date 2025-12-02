2432658e15091f387337c05dc21970dd283d54e0
Caiojaqueri <caiojaqueri.contato@gmail.com>
2025-12-02 17:21:19 -0300

    fiz uma estrutura listanto todas as estações que serão usadas

--- docs/metro.c
+++ docs/metro.c
@@ -2,15 +2,36 @@
 
 #include <stdio.h> // vou usar para entrada e saída de dados
 #include <string.h> // vou usar para manipulação de strings
+#include <windows.h> // vou usar para simular o tempo de espera nas estações
 #include <time.h> // vou usar para simular o tempo de espera nas estações
 
-#define MAX_ESTACOES 10 
-#define ESTACAO_FINAL 9
-#define TEMPO_ESPERA 15 
+#define MAX_ESTACOES 12 
+#define ESTACAO_FINAL 1
+#define TEMPO_ESPERA 5 
 
 #define PORTA_ABERTA 1
 #define PORTA_FECHADA 0
 
+typedef struct {
+    int numero;
+    char nome[50];
+} estacao;
+
+estacao metro[MAX_ESTACOES] = {
+    {0, "Vila Prudente"},
+    {1, "Tamanduateí"},
+    {2, "Sacomã"},
+    {3, "Alto do ipiranga"},
+    {4, "Santos-Imigrantes"},
+    {5, "Chácara Klabin"},
+    {6, "Ana Rosa"},
+    {7, "Paraíso"},
+    {8, "Brigadeiro"},
+    {9, "Trianon-Masp"},
+    {10, "Consolação"},
+    {11, "Clínicas"},
+    {12, "Vila Madalena"},
+};
 
 int estacao_atual = 0;
 int proxima_estacao = 1;
@@ -18,7 +39,7 @@ int estado_porta = PORTA_FECHADA;
 
 void abrirPorta() {
     estado_porta = PORTA_ABERTA;
-    printf("Porta aberta na estação %d\n", estacao_atual);
+    printf("Porta aberta na estação %s\n", metro[estacao_atual].nome);
 }
 
 void fechar_porta() {
@@ -29,13 +50,13 @@ void fechar_porta() {
 void parar_estacao() {
     printf("Você chegou na estação %d\n", estacao_atual);
     abrirPorta();
-    sleep(TEMPO_ESPERA);
+    Sleep(TEMPO_ESPERA);
     fechar_porta();
 }
 
 void movimento() {
     printf("Indo para a próxima estação: %d\n", estacao_atual + 1);
-    sleep(5); // simula o tempo de viagem entre as estações
+    Sleep(5); // simula o tempo de viagem entre as estações
 }
 
 void linha() {
@@ -47,9 +68,6 @@ void linha() {
     printf("Você chegou na estação final: %d\n", ESTACAO_FINAL);
     parar_estacao(); //parada final
 }
-
-
-
 
 /* 
 por enquanto eu penso em fazer a lógica dessa forma: 
@@ -65,6 +83,12 @@ siga para a próxima estação
  */ 
 
 int main() {
+    printf("Bem vindo ao metrô de São Paulo!\n");
+
+    linha();
+
+    printf("Obrigado por usar o metrô de São Paulo!\n");
+
 
     return 0;
 }
